6833e9a93f74ca4ba029063ac6b3a2945c05efa6
Ian Hartley <Ian.Hartley@tfg-brands.co.uk>
2022-10-30 17:03:16 +0000

    Mark item as complete

--- todo_app/app.py
+++ todo_app/app.py
@@ -5,6 +5,7 @@ from flask import redirect
 
 from todo_app.flask_config import Config
 from todo_app.data.session_items import get_items, add_item
+from todo_app.data.session_items import get_item, save_item
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -20,6 +21,19 @@ def additem():
     add_item(request.form.get('title'))
     return redirect('/') 
 
+#Add mark item as complete
+@app.route('/complete', methods=['POST'])
+def complete():
+    id_to_complete = request.form.get('ID')
+    item_to_complete = get_item(id_to_complete)
+    if item_to_complete is None:
+        return redirect('/')
+    else:
+        item_to_complete.update({'status':'Complete'})
+        save_item(item_to_complete) 
+        return redirect('/')
+       
+    
 if __name__ == '__main__':
     app.run
     
